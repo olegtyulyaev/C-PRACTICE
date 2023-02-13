153cdd2ac9c543f1958c72dc97b606c7646e66cd
oleg_tyulyaev <tyulyaevoleg@gmail.com>
2023-02-13 23:39:45 +0300

    добавлено решение Задачи4 из HW1

---
+++ HW1-4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+Console.Clear();
+Console.Write("Введите число 1:  ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2:  ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите число 3:  ");
+int c = int.Parse(Console.ReadLine());
+if (a>b && a>c)
+{
+Console.WriteLine($"{a} -> наибольшее из 3х введенных чисел");
+} 
+else if (b > c)
+{
+Console.WriteLine($"{b} -> наибольшее из 3х введенных чисел");
+}
+else
+{
+Console.WriteLine($"{c} -> наибольшее из 3х введенных чисел");
+}
