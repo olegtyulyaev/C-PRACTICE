3b960f1aa39a34bfac4d70127629dba5e6e8eb5e
oleg_tyulyaev <tyulyaevoleg@gmail.com>
2023-02-14 21:51:21 +0300

    добавлено решение Задачи7 из HW1

---
+++ HW1-7/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int num=int.Parse(Console.ReadLine());
+int y=num%10;
+Console.WriteLine($"{y}");
