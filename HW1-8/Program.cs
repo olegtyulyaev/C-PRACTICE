616e84b72f8540ad2c17244473886a1dbf1ad88b
oleg_tyulyaev <tyulyaevoleg@gmail.com>
2023-02-14 21:44:01 +0300

    добавлено решение Задачи8 из HW1

---
+++ HW1-8/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+Console.Clear();
+Console.WriteLine("Введите число :  ");
+int N = int.Parse(Console.ReadLine());
+    
+for (int i = 2; i <= N; i += 2)
+Console.WriteLine(i);
+
