a7e064cedb2ae24584ceb707917ff667dd9ebbda
oleg_tyulyaev <tyulyaevoleg@gmail.com>
2023-02-16 22:52:55 +0300

    Добавлено решение Задачи10 из HW2 - логическое

---
+++ HW2-10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.Write("Введи трёхзначное число: ");
+int Number =  int.Parse(Console.ReadLine());
+string stringNumber = Convert.ToString(Number);
+Console.WriteLine("вторая цифра введенного числа - "+stringNumber[1]);
+
