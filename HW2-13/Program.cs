2b0f69822f54633d8959ea23935a7e5e5fb3a9ce
oleg_tyulyaev <tyulyaevoleg@gmail.com>
2023-02-18 13:03:58 +0300

    добавлено решение Задачи13 из HW2

---
+++ HW2-13/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введи число: ");
+  
+int RandomDigit = int.Parse(Console.ReadLine());
+string RandomText = Convert.ToString(RandomDigit);
+if (RandomText.Length > 2){
+ Console.WriteLine("третья цифра -> " + RandomText[2]);
+}
+else {
+ Console.WriteLine("-> третьей цифры нет");
+}
