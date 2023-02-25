5f06cd4b22a8507423b85680132d89ac446529a7
olegtyulyaev <124436130+olegtyulyaev@users.noreply.github.com>
2023-02-25 14:01:55 +0300

    Добавлено решение Задачи27 из HW4

---
+++ HW4-27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.Write("Введите число: ");
+ int num = int.Parse(Console.ReadLine());
+
+static int GetSum (int value)
+{
+    if (value <10 )
+    return value;
+
+    int digit = value % 10;
+    int nextValue = value / 10;
+    return digit + GetSum(nextValue);
+}
+
+Console.WriteLine($"Сумма цифр = {GetSum(num)}");
