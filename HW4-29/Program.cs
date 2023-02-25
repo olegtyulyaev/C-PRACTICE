71bce44cd36511217f0fe0a6cb724470507ced56
olegtyulyaev <124436130+olegtyulyaev@users.noreply.github.com>
2023-02-25 17:22:26 +0300

    Добавлено решение Задачи29 из HW4

---
+++ HW4-29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//   6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+Console.Write("Введите желаемую длинну массива: ");
+Int32 enterNumber = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[enterNumber];
+
+for (int i = 0; i < enterNumber; i++)
+{
+array[i] = new Random().Next(enterNumber+1);
+ Console.Write(array[i] + "; ");
+}
+Console.WriteLine();
