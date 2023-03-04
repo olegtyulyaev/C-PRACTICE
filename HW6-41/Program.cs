19c23090526ad60e1c515f62ca193b0a67a701ea
olegtyulyaev <124436130+olegtyulyaev@users.noreply.github.com>
2023-03-04 18:35:19 +0300

    Добавлено решение Задачи41 из HW6

---
+++ HW6-41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 41: 
+// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+    Console.Clear();
+    string randomNumbers = ReadDataStr("Введите последовательность чисел через запятую: ");
+    int countPosNum = CountPositivNumber(randomNumbers);
+    Console.WriteLine("Количество положительных чисел: " + countPosNum);
+
+    // Метод читает данные от пользователя
+    string ReadDataStr(string msg)
+    {
+        Console.WriteLine(msg);
+        return Console.ReadLine() ?? "0";
+    }
+
+    //Метод считает кол-во положительных элементов
+    int CountPositivNumber(string str)
+    {
+        int count = 0;
+        string strWoSp = str.Replace(" ", "");
+        string[] strArr = strWoSp.Split(",");
+        int[] intArr = new int[strArr.Length];
+
+        for (int i = 0; i < intArr.Length; i++) intArr[i] = int.Parse(strArr[i]);
+        for (int i = 0; i < intArr.Length; i++) if (intArr[i] > 0) count++;
+
+        return count;
+ }
+
