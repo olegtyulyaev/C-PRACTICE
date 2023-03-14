871e4dccb8163465f0f03706acfcf202721fd4c5
olegtyulyaev <124436130+olegtyulyaev@users.noreply.github.com>
2023-03-14 21:37:42 +0300

    Добавлено решение Задачи54 из HW8

---
+++ HW8-54/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+
+using System;
+using static System.Console;
+
+Clear();
+
+
+int row = ReadData("Количество строк матрицы: ");
+int column = ReadData("Количество столбцов матрицы: ");
+int range = ReadData("Диапазон элементов массива: ");
+
+
+int[,] arr2D = Gen2DArrayInt(row, column, range);
+Console.WriteLine();
+Console.WriteLine("Исходный 2D массив: ");
+Print2DArr(arr2D);
+
+SortLines(arr2D);
+
+Console.WriteLine();
+Console.WriteLine("Отсортированный 2D массив: ");
+Print2DArr(arr2D);
+
+
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Функция для заполнения 2D массива 
+int[,] Gen2DArrayInt(int row, int column, int range)
+{
+    int i = 0; int j = 0;
+    int[,] arr = new int[row, column];
+    while (i < row)
+    {
+        j = 0;
+        while (j < column)
+        {
+            arr[i, j] = new Random().Next(0, range);
+            j++;
+        }
+        i++;
+    }
+    return arr;
+}
+
+// Сортировка пузырьковым методом
+void SortLines(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void Print2DArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
