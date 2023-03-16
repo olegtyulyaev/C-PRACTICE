473ba0ea9bf1ce6ee0f9b52fb8b5f61de6251c58
olegtyulyaev <124436130+olegtyulyaev@users.noreply.github.com>
2023-03-16 22:36:34 +0300

    Добавлено решение Задачи64 из HW9

---
+++ HW9-64/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+using System;
+using static System.Console;
+
+Clear();
+
+int number = ReadData("Введите число N: ");
+string resultLine = LineGenRec(number);
+PrintResult(resultLine);
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+void PrintResult(string prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+// Функция рекурсии
+string LineGenRec(int num)
+{
+    if (num == 0)
+    {
+        return string.Empty;
+    }
+    else
+    {
+        return num + " " + LineGenRec (num - 1);
+    }
+}
