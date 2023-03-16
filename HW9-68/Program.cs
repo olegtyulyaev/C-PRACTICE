b5b0f648c9cfeff5969b4c848bfbe2fbe83302cd
olegtyulyaev <124436130+olegtyulyaev@users.noreply.github.com>
2023-03-16 22:52:35 +0300

    Добавлено решение Задачи68 из HW9

---
+++ HW9-68/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+using System;
+using static System.Console;
+
+Clear();
+
+
+int n = ReadData("Введите число N: ");
+int m = ReadData("Введите число M: ");
+int sumAckerman = Ackerman(n, m);
+PrintData("значение функции Аккермана равно: ", sumAckerman);
+
+// Метод рекурсии вычисляет функцию Аккермана
+int Ackerman(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Ackerman(n - 1, 1);
+    else
+      return Ackerman(n - 1, Ackerman(n, m - 1));
+}
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Метод выводит результат пользователю
+void PrintData(string msg, int res)
+{
+    Console.WriteLine(msg + res);
+}
